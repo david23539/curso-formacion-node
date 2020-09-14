60d0b3e27f0ac2ae371c07f2c0663e210df33331
david <david23539@gmail.com>
2020-09-14 18:57:39 +0200

    se añade proyecto de nest

--- node-nest/node-nest/src/app.controller.ts
+++ node-nest/node-nest/src/app.controller.ts
@@ -1,5 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Ip,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { ProductDto } from './product.dto';
 
 @Controller()
 export class AppController {
@@ -8,5 +17,26 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post()
+  addCreateProduct(): string {
+    return 'Se han añadido todos los productos';
+  }
+
+  @Post('/addProduct')
+  @HttpCode(201)
+  addCreateProduct2(@Body() productDto: ProductDto): string {
+    return `El nombre del producto es ${productDto.name} con un precio de ${productDto.price} y un tamaño ${productDto.size}`;
+  }
+
+  @Get(':id')
+  getProduct(@Param() params): string {
+    return params.id;
+  }
+
+  @Get(':id')
+  getProduct2(@Param() params, @Ip() ip): string {
+    return ip;
   }
 }
---
+++ node-nest/node-nest/src/product.dto.ts
@@ -0,0 +1,5 @@
+export class ProductDto {
+  name: string;
+  price: number;
+  size: number;
+}
